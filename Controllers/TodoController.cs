4a31c38088d0dce7bbedbbb1964b467835eca8ea
Mikko Keinänen <mikko.keinanen@saimasoft.fi>
2018-09-06 09:50:10 +0300

    Added get methods to TodoController

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -23,5 +23,22 @@ namespace TodoApi.Controllers
                 _context.SaveChanges();
             }
         }
+
+        [HttpGet]
+        public ActionResult<List<TodoItem>> GetAll()
+        {
+            return _context.TodoItems.ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public ActionResult<TodoItem> GetById(long id)
+        {
+            var item = _context.TodoItems.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
     }
 }
